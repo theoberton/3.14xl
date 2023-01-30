78bdb8446eac72279953d9153061a42b2a1e16df
Pavel Kalugin <p.kalugin@paralect.com>
2023-01-30 11:33:29 +0300

    IPFS example page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Ipfs from './pages/Ipfs';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Ipfs />
   </React.StrictMode>,
 )
