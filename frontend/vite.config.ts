27c37779dc37c3e790d8a3b1e28f30f4b20ca2e6
Egor Mozheiko <gopherbopher123@gmail.com>
2023-02-08 18:09:48 +0300

    add vite base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,14 @@ import path from 'node:path';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/3.14xl/',
 	define: {
 		global: 'window', // Polyfill for ipfs-core
 	},
 	resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	plugins: [react(), nodePolyfills()],
 });
