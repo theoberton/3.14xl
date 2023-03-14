9cd0ec3edceb3e2a9629b1da9dc7b2be1df7fbb1
Nick <kolyanbestele@gmail.com>
2023-03-14 12:55:35 +0300

    Fix invalid form disable

--- frontend/src/pages/CreateEdition/FormArea.tsx
+++ frontend/src/pages/CreateEdition/FormArea.tsx
@@ -23,7 +23,7 @@ export function FormArea({
 	deploymentState,
 }: Props) {
 	const tonConnectAddress = useTonAddress();
-	const { isValid, dirty, values, touched, setFieldValue, isSubmitting, submitForm, resetForm } =
+	const { isValid, dirty, values, touched, setFieldValue, errors, isSubmitting, submitForm, resetForm } =
 		useFormikContext<FormValues>();
 
 	const isFormValid: boolean = isValid && dirty && Boolean(tonConnectAddress);
@@ -104,7 +104,7 @@ export function FormArea({
 					<Button
 						componentType="button"
 						buttonType="submit"
-						disabled={isFormValid}
+						disabled={!isFormValid}
 						expanded
 						isSubmitting={isSubmitting}
 						kind={ButtonKinds.basic}
