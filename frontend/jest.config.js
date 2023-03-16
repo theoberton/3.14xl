3ac34c0d2de229aae493d4edea36d11c5e48679f
Pavel Kalugin <p.kalugin@paralect.com>
2023-03-16 09:18:30 +0300

    Fix test launch

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
