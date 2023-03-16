2e7afefe0567930bf5bfdacdeee6fa36871248b6
kazeens <31125887+kazeens@users.noreply.github.com>
2023-03-16 18:01:08 +0300

    Add hardcoded values on landing  mainnet (#104)

---
+++ frontend/src/pages/Landing/showcaseEdition.ts
@@ -0,0 +1,61 @@
+import { ManagerContract } from '@/libs/apiClient/types';
+
+export const showcaseEditions: ManagerContract[] = [
+	{
+		contractAddress: 'EQC0ieV40auqODkKtVZJUBSsXOASbo81n5IKcfQnvuybthip',
+		ownerAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		collectionAddress: 'EQBcP6raHhdPeLJ8KRI0SQ669k5sh4aQ876u97BFWEdeDq9a',
+		overviewData: {
+			content:
+				'https://gateway.ipfscdn.io/ipfs/QmYXt3zGWmvGgX3ZizoqzkGdcwAKYqj6UxYox4yvy2ZJWU/27B28FD0-F9EF-4935-B204-864697A25BFA.jpeg',
+			limit: 0,
+			name: 'ACCEPTANCE',
+			owner: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+			price: '0',
+			minted: 0,
+			collectionAddress: 'EQBcP6raHhdPeLJ8KRI0SQ669k5sh4aQ876u97BFWEdeDq9a',
+			dateEnd: 0,
+			dateStart: 0,
+			maxSupply: '0',
+			payoutAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		},
+	},
+	{
+		contractAddress: 'EQArtFJPDbTnalSttGxWrh33cuyNHD-xbEsTU6uxqLq4Dkaw',
+		ownerAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		collectionAddress: 'EQBAC8V-hS4cAOA7bbiaLgjkmDZWm-mBnuCpY8mShzYnC3dY',
+		overviewData: {
+			content:
+				'https://gateway.ipfscdn.io/ipfs/QmRTZHrnFYx6QUNAJU5RNYuZHiszWbj8K1dkm3pTNtEU5H/D52B4AAF-FD16-4DF0-90BC-163509C07A9C.jpeg',
+			limit: 0,
+			name: 'COMPASSION',
+			owner: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+			price: '0',
+			minted: 0,
+			collectionAddress: 'EQBAC8V-hS4cAOA7bbiaLgjkmDZWm-mBnuCpY8mShzYnC3dY',
+			dateEnd: 0,
+			dateStart: 0,
+			maxSupply: '0',
+			payoutAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		},
+	},
+	{
+		contractAddress: 'EQDzL-e7Em-1THfhWpd3QIXDjvUviEBLs2_x8R1GicjC2MSC',
+		ownerAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		collectionAddress: 'EQD-gNcqvQs9q_z5EeG-9P9WanoWVSzVSowSnXAV8rfk-E_H',
+		overviewData: {
+			content:
+				'https://gateway.ipfscdn.io/ipfs/QmV7VEzf7C3FD5YLXQqAFeCVDj43nek2BJ3G5QEqd9Ujw3/9032E0EC-6C9E-4A93-90DB-CCBD20FBC71F.jpeg',
+			limit: 0,
+			name: 'CREATION',
+			owner: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+			price: '0',
+			minted: 0,
+			collectionAddress: 'EQD-gNcqvQs9q_z5EeG-9P9WanoWVSzVSowSnXAV8rfk-E_H',
+			dateEnd: 0,
+			dateStart: 0,
+			maxSupply: '0',
+			payoutAddress: 'EQBHueQiqdMZUutIQP4dN979yc3_ZkfxT_dYr91YZSksRIyA',
+		},
+	},
+];
