b6de070ffdc2f95899f3becf2d18b7561b10c158
Pavel Kalugin <ptashka.kl@gmail.com>
2023-08-24 23:51:54 +0300

    feat: add archived banner

--- src/components/Layout/Page/index.tsx
+++ src/components/Layout/Page/index.tsx
@@ -5,10 +5,14 @@ import styles from './styles.module.scss';
 
 export function Page({ footer = false }: { footer?: boolean }) {
 	return (
-		<div className={styles.pageContainer}>
-			<Header />
-			<Outlet />
-			{footer && <Footer />}
-		</div>
+		<>
+			<div className={styles.warningBanner}>The project is archived and offered as is. Use at your own risk.</div>
+			<div className={styles.pageContainer}>			
+				
+				<Header />
+				<Outlet />
+				{footer && <Footer />}
+			</div>
+		</>
 	);
 }
